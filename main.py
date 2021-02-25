b6059fe662e492cd0d24db3fe75e2326a0a139b7
adamkalista111 <adamkalista111@gmail.com>
2021-02-25 11:08:17 +0100

    do 4

--- main.py
+++ main.py
@@ -1,4 +1,59 @@
-
+#zad 1
+n1='napis'
+n2='napis2'
+print(n1)
+print(n2)
+calkowita = 7
+rzeczywista = 7.5
+print(calkowita)
+print(rzeczywista)
+#zad 2
+from math import *
 a=2
-
+b=3
+dodawanie= a+b
+odejmowanie= a-b
+dzielenie=b/a
+dzielenie_calkowite= b//a
+mnozenie= a*b
+potegowanie= a**3
+resztazdzielenia= 4%3
+print(sqrt(b))
+print(dodawanie)
+print(odejmowanie)
+print(dzielenie)
+print(dzielenie_calkowite)
+print(mnozenie)
+print(potegowanie)
+print(resztazdzielenia)
+#zad=3
+a+=2
 print(a)
+a-=2
+print(a)
+a/=2
+print(a)
+a*=2
+print(a)
+a**=2
+print(a)
+a%=2
+print(a)
+a-=2
+print(a)
+a/=2
+print(a)
+a*=8
+print(a)
+a**=2
+print(a)
+a%=5
+print(a)
+#zadanie 4
+e= exp(1)
+e**=10
+
+x=(log(5 + sin(8)**2))
+f=pow(x,1/6)
+print(f)
+
